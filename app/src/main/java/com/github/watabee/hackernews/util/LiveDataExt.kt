11cf8733dfa14143481ca834e1c2c0622322d544
watabee <watabee.dev@gmail.com>
2019-03-31 23:39:05 +0900

    Delete unused code

--- app/src/main/java/com/github/watabee/hackernews/util/LiveDataExt.kt
+++ app/src/main/java/com/github/watabee/hackernews/util/LiveDataExt.kt
@@ -4,10 +4,6 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 
-inline fun <T> LiveData<T>.observe(owner: LifecycleOwner, crossinline onChanged: (T?) -> Unit) {
-    observe(owner, Observer { onChanged(it) })
-}
-
 inline fun <T> LiveData<T>.observeNonNull(
     owner: LifecycleOwner,
     crossinline onChanged: (T) -> Unit

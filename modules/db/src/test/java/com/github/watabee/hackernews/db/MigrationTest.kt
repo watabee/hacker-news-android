7e86f584aaad28754c608482b13f6a69cfcd61c1
watabee <watabee.dev@gmail.com>
2019-04-23 18:31:41 +0900

    Add migration test

---
+++ modules/db/src/test/java/com/github/watabee/hackernews/db/MigrationTest.kt
@@ -0,0 +1,33 @@
+package com.github.watabee.hackernews.db
+
+import androidx.room.testing.MigrationTestHelper
+import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+private const val TEST_DB = "migration-test"
+
+@RunWith(AndroidJUnit4::class)
+class MigrationTest {
+
+    @get:Rule
+    val helper: MigrationTestHelper = MigrationTestHelper(
+        InstrumentationRegistry.getInstrumentation(),
+        AppDatabase::class.java.canonicalName,
+        FrameworkSQLiteOpenHelperFactory()
+    )
+
+    @Test
+    @Throws(IOException::class)
+    fun migrate1To2() {
+        var db = helper.createDatabase(TEST_DB, 1).apply {
+            close()
+        }
+
+        db = helper.runMigrationsAndValidate(TEST_DB, 2, true, AppDatabase.MIGRATION_1_2)
+    }
+}
